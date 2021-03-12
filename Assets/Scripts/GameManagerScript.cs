d892b7db0e230a03d29536dc3e7b6d60c5e511ea
Alex Belyaevskiy <alexbel787@yahoo.com>
2021-03-12 18:08:16 +0300

    init commit

---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public float levelHeight;
+    public GameObject cubePrefab;
+    private GameObject player;
+    private Transform envT;
+    private Camera cam;
+    private Vector3 camOffset;
+
+    public Text ScoreText;
+    public GameObject GameOverObj;
+
+    private void Start()
+    {
+        // Adjust UI and camera view to fit screen size.
+        float screenRatio = (float)Screen.height / Screen.width; 
+        if (screenRatio < 1.7f)
+            Camera.main.GetComponent<CameraConstantWidth>().WidthOrHeight = 1f;
+        else if (screenRatio > 1.95f)
+            GameObject.Find("Canvas").GetComponent<CanvasScaler>().matchWidthOrHeight = .6f;
+
+        player = GameObject.Find("Worm");
+        envT = GameObject.Find("Environment").transform;
+        cam = Camera.main;
+        camOffset = cam.transform.position - player.transform.position;
+
+        LevelGenerator();
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 camTargetPos = player.transform.position + camOffset;
+        cam.transform.position = camTargetPos;
+    }
+
+    private void LevelGenerator()
+    {
+        float length = 0;
+        for (float height = 0; height <= levelHeight;)
+        {
+            Vector3 rotation = Vector3.zero;
+            if (Random.value < .5f)
+                rotation += new Vector3(0, 0, Random.Range(10, 90));
+            GameObject prefab = cubePrefab;
+            prefab.transform.localScale = new Vector3(Random.Range(.1f, 1), .1f, 3);
+            GameObject step = Instantiate(prefab, new Vector3(length, height, -1.5f) + new Vector3(prefab.transform.localScale.x / 2, 
+                prefab.transform.localScale.y / 2, 0), Quaternion.Euler(rotation), envT);
+            step.GetComponent<MeshRenderer>().material.color = new Color(rotation.z / 90, 1 - rotation.z / 90, 0);
+            length = step.GetComponent<MeshRenderer>().bounds.max.x;
+            height = step.GetComponent<MeshRenderer>().bounds.max.y;
+        }
+    }
+
+    public void GameOver()
+    {
+        GameOverObj.SetActive(true);
+    }
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/WormScript.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WormScript : MonoBehaviour
+{
+    public float power;
+    public float maxDrag = 5f;
+    public Rigidbody rb;
+    public LineRenderer lr;
+
+    private Vector3 dragStartPos;
+    private Vector3 touchPos;
+    private Vector3 clampedForce;
+    public bool grounded = true;
+    private bool onGround;
+    private bool doOnce;
+    private bool showTrajectory;
+
+    private GameManagerScript GMS;
+
+    private void Start()
+    {
+        rb.sleepThreshold = 0;
+        GMS = GameObject.Find("GameManager").GetComponent<GameManagerScript>();
+    }
+
+    private void Update()
+    {
+        if (grounded)
+        {
+#if UNITY_ANDROID
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                touchPos = touch.position;
+                touchPos.z = 10;
+
+                if (touch.phase == TouchPhase.Began)
+                {
+                    DragStart();
+                }
+
+                if (touch.phase == TouchPhase.Ended)
+                {
+                    DragRelease();
+                }
+            }
+#endif
+
+#if UNITY_EDITOR
+            if (Input.GetMouseButtonDown(0))
+            {
+                touchPos = Input.mousePosition;
+                touchPos.z = 10;
+                DragStart();
+            }
+
+            if (Input.GetMouseButton(0))
+            {
+                touchPos = Input.mousePosition;
+                touchPos.z = 10;
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                touchPos = Input.mousePosition;
+                touchPos.z = 10;
+                DragRelease();
+            }
+#endif
+
+            if (showTrajectory) ShowTrajectory();
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Steps") && !SoundManager.instance.GetComponent<SoundManager>().soundSource.isPlaying)
+        {
+            GMS.ScoreText.text = "ALTITUDE " + (int)transform.position.y;
+            if (rb.velocity.magnitude > .6f)
+                SoundManager.instance.RandomizeSfx(.6f, SoundManager.instance.smackSounds);
+        }
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            SoundManager.instance.RandomizeSfx(1, SoundManager.instance.fallSounds);
+            GMS.GameOver();
+            this.enabled = false;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Steps"))
+        {
+            onGround = true;
+            if (!doOnce)
+            {
+                doOnce = true;
+                StartCoroutine(CheckOnGroundTimer());
+            }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Steps"))
+        {
+            onGround = false; // If jumped, bouncing or rolling with no ground touch - then player can't jump
+        }
+    }
+
+    private IEnumerator CheckOnGroundTimer()
+    {
+        yield return new WaitForSeconds(.5f);
+        if (onGround) grounded = true;
+        doOnce = false;
+    }
+
+    private void DragStart()
+    {
+        dragStartPos = Camera.main.ScreenToWorldPoint(touchPos);
+        dragStartPos.z = 0;
+        showTrajectory = true;
+    }
+
+    private void DragRelease()
+    {
+        grounded = false;
+        showTrajectory = false;
+        lr.positionCount = 0;
+        rb.AddForce(clampedForce, ForceMode.Impulse);
+        SoundManager.instance.RandomizeSfx(1, SoundManager.instance.jumpSounds);
+    }
+
+    private void ShowTrajectory()
+    {
+        Vector3 draggingPos = Camera.main.ScreenToWorldPoint(touchPos);
+        draggingPos.z = 0;
+        Vector3 force = dragStartPos - draggingPos;
+        clampedForce = Vector3.ClampMagnitude(force, maxDrag) * power;
+        print("force " + force + " / " + "clampedForce " + clampedForce);
+        Vector3[] points = new Vector3[10];
+        lr.positionCount = points.Length;
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            float time = i * 0.1f;
+
+            points[i] = transform.position + clampedForce / power * time + Physics.gravity * time * time / 2f;
+
+            if (points[i].y < 0)
+            {
+                lr.positionCount = i + 1;
+                break;
+            }
+        }
+
+        lr.SetPositions(points);
+    }
+
+}
